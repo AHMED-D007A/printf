05d160b37738d978ff684e547d28a503d8656252
LeHonda1 <Honda56444@outlook.com>
2023-03-26 14:13:24 -0700

    fixing betty

--- task-1_cases.c
+++ task-1_cases.c
@@ -1,34 +1,32 @@
-#include<stdarg.h>
+#include <stdarg.h>
 #include "main.h"
-int case_num(va_list ap) {
+
+/**
+ * case_num - handles conversion specifier 'd'
+ * @ap: list of arguments
+ *
+ * Return: number of characters printed
+ */
+int case_num(va_list ap)
+{
     int i = 0, sign = 0;
-    /*
-     * allocate 10 bytes as max int is 10 chars (2,147,483,647)
-     * and 1byte for '-' sign if needed
-     * also 1byte for the NULL character
-     */
-    char info[12];
-    /* read next _printf function argument as an int */
+    char info[12]; /* max int is 10 chars (2,147,483,647) */
     int val = va_arg(ap, int);
-    
-    /* check if val is an negative number */
-    if (val < 0) {
+
+    if (val < 0)
+    {
         val = -val;
         sign = 1;
         info[i++] = '-';
     }
-    
-    /* convert digits into ascii string */
-    while (val || !i) {
+
+    while (val || !i)
+    {
         info[i++] = val % 10 + '0';
         val /= 10;
     }
-    info[i] = 0; /* set null character at the end */
-    
-    /*
-     * reverse the parsed characters
-     * because the code parsed the digits in reverse order
-     */
+    info[i] = '\0';
+
     str_rev(info + sign, i - sign - 1);
     return _putstring(info);
 }
