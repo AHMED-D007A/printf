33adcee7120f4d99c4ce548b581049acf9f4ea12
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-03-26 14:28:56 -0700

    Fixing

--- task-1_cases.c
+++ task-1_cases.c
@@ -1,33 +1,31 @@
-#include <stdarg.h>
 #include "main.h"
-
 /**
- * case_num - handles conversion specifier 'd'
- * @ap: list of arguments
- *
- * Return: number of characters printed
- */
+* case_num - handles conversion specifier 'd'
+* @ap: list of arguments
+*
+* Return: number of characters printed
+*/
 int case_num(va_list ap)
 {
-    int i = 0, sign = 0;
-    char info[12]; /* max int is 10 chars (2,147,483,647) */
-    int val = va_arg(ap, int);
+	int i = 0, sign = 0;
+	char info[12];
 
-    if (val < 0)
-    {
-        val = -val;
-        sign = 1;
-        info[i++] = '-';
-    }
+	int val = va_arg(ap, int);
 
-    while (val || !i)
-    {
-        info[i++] = val % 10 + '0';
-        val /= 10;
-    }
-    info[i] = '\0';
+	if (val < 0)
+	{
+		val = -val;
+		sign = 1;
+		info[i++] = '-';
+	}
 
-    str_rev(info + sign, i - sign - 1);
-    return _putstring(info);
+	while (val || !i)
+	{
+		info[i++] = val % 10 + '0';
+		val /= 10;
+	}
+	info[i] = '\0';
+
+	str_rev(info + sign, i - sign - 1);
+	return _putstring(info);
 }
-
